10a1d86972b05973a685e9d3ec63e4156894b6c5
AndreLeEEEEEE <78389112+AndreLeEEEEEE@users.noreply.github.com>
2022-11-03 10:44:24 -0600

    Create Weapon.py

---
+++ Items/Weapons/Weapon.py
@@ -0,0 +1,23 @@
+class Weapon:
+    def __init__(self):
+        self.name = ""
+        self.MT = 0
+        self.HIT = 0
+        self.RNG = 0
+        self.offence = True
+        self.type = ""
+
+    def getName(self):
+        return self.name
+
+    def getMT(self):
+        return self.MT
+
+    def getHIT(self):
+        return self.HIT
+        
+    def getRNG(self):
+        return self.RNG
+
+    def useWeapon(self, charMT):
+        return charMT + self.MT
