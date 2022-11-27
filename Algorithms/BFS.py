df5e665b577ec0b5676582aab0682130dd24c750
Andre Le <AndreLeEEEEEE>
2022-11-27 09:49:33 -0700

    Fix BFS scan to not target self when finding allies

--- Algorithms/BFS.py
+++ Algorithms/BFS.py
@@ -1,8 +1,10 @@
 from Directions import *
 
-def scan(field, tile, target, RNG):
+def scan(field, unit, tile, target, RNG):
     """From the current tile, scan for targets in range."""
     """
+    field - list of list of int/char, the map
+    unit - tuple of int, the tile unit is currently on
     tile - tuple of int, a tile the unit could stand on
     target - int, an enemy to attack or an injured ally to heal
     RNG - tuple of int, the range at which a unit can attack
@@ -11,41 +13,41 @@ def scan(field, tile, target, RNG):
     for num in RNG:
         if num == 1:
             try: 
-                if field[r+1][c] == target: return True
+                if (r+1,c) != unit and field[r+1][c] == target: return True
             except: pass
             try:
-                if field[r-1][c] == target: return True
+                if (r-1,c) != unit and field[r-1][c] == target: return True
             except: pass
             try:
-                if field[r][c+1] == target: return True
+                if (r,c+1) != unit and field[r][c+1] == target: return True
             except: pass
             try:
-                if field[r][c-1] == target: return True
+                if (r,c-1) != unit and field[r][c-1] == target: return True
             except: pass
         else:
             try:
-                if field[r+2][c] == target: return True
+                if (r+2,c) != unit and field[r+2][c] == target: return True
             except: pass
             try:
-                if field[r-2][c] == target: return True
+                if (r-2,c) != unit and field[r-2][c] == target: return True
             except: pass
             try:
-                if field[r][c+2] == target: return True
+                if (r,c+2) != unit and field[r][c+2] == target: return True
             except: pass
             try:
-                if field[r][c-2] == target: return True
+                if (r,c-2) != unit and field[r][c-2] == target: return True
             except: pass
             try:
-                if field[r+1][c+1] == target: return True
+                if (r+1,c+1) != unit and field[r+1][c+1] == target: return True
             except: pass
             try:
-                if field[r-1][c+1] == target: return True
+                if (r-1,c+1) != unit and field[r-1][c+1] == target: return True
             except: pass
             try:
-                if field[r+1][c-1] == target: return True
+                if (r+1,c-1) != unit and field[r+1][c-1] == target: return True
             except: pass
             try:
-                if field[r-1][c-1] == target: return True
+                if (r-1,c-1) != unit and field[r-1][c-1] == target: return True
             except: pass
     return False
 
@@ -64,7 +66,7 @@ def BFS(field, unit, target, RNG):
         toBeQueued = []
         for tile in queue:
             print("From this tile", tile)
-            if scan(field, tile, target, RNG):
+            if scan(field, unit, tile, target, RNG):
                     return tile
             appRules = applicable_rules(field, tile)
             for rule in appRules:
@@ -79,8 +81,8 @@ def BFS(field, unit, target, RNG):
     return "No target found"
 
 field = [['_', '_', '_', '_', '_'],
-        ['_', '_', '_', '_', 2],
-        ['_', '_', 'U', '_', '_'],
+        ['_', '_', '_', '_', 1],
+        ['_', '_', 1, '_', '_'],
         ['_', '_', '_', '_', '_'],
         ['_', '_', '_', '_', '_']]
 
@@ -91,6 +93,7 @@ field = [['_', '_', '_', '_', '_'],
 #         ['_', '_', 'V', '_', '_']]
 
 # Find the closest enemy
-print("Final result:", BFS(field, (2, 2), 2, [1, 2]))
+#print("Final result:", BFS(field, (2, 2), 2, [1, 2]))
 
-# Find the closest ally
+# Find the closest ally, make sure to not "find" itself
+print("Final result:", BFS(field, (2, 2), 1, [1]))
